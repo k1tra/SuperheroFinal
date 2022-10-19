fa64646eeab209139478ef6afd46802857a8f228
k1tra <jepp880d@stud.kea.dk>
2022-10-19 19:38:58 +0200

    HAR GJORT SÅ MAN KAN FINDE EN HELT DEN FØRSTE HELT PÅ EN LSITE

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,14 +1,36 @@
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 
 
 public class Database {
     ArrayList<Superhero> superheroes = new ArrayList<>();
 
     // createSuperHero
-    public void createSuperhero(String name, String superheroname, boolean isHuman, int creationYear, double strength){
-        Superhero superhero = new Superhero(name,superheroname,isHuman,creationYear,strength);
+    public void createSuperhero(String name, String superheroname, boolean isHuman, int creationYear, double strength) {
+        Superhero superhero = new Superhero(name, superheroname, isHuman, creationYear, strength);
         superheroes.add(superhero);
         // System.out.println(superheroes);
+        
+    }
+
+    // Hvordan laver man denne metode? =)
+    public void searchSuperhero(String search) {
+        Boolean found = false;
+
+        for (Superhero superhero : superheroes) {
+            if (superhero.getName().toLowerCase().contains(search.toLowerCase())) {
+                System.out.println("Superhelten er fundet: "+superhero.getName()+superhero.getSuperheroName()+superhero.getIsHuman()+superhero.getCreationYear()+superhero.getStrength());
+                break;
+
+            } if(!found){
+                System.out.println("Kunne ikke finde superhelten");
+
+            }
+
+        }
+
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,28 +18,29 @@ public class Main {
 
         do {
             System.out.println("1. Opret superhelt");
+            System.out.println("2. Søg på specifik superhelt");
             System.out.println("5. Se listen over dine superhelte");
             System.out.println("9. Afslut programmet");
 
 
             valg = sc.nextInt();
             if (valg == 1) {
-
+                // CREATION OF A SUPERHERO
                 System.out.println("Hvad er dens navn?");
-                // STADIG PROBLEMER MED nextLine som antager værdien af den næste angivet String -- SPØRG ZACG
+                // STADIG PROBLEMER MED nextLine som antager værdien af den næste angivet String -- SPØRG ZACH
                 String navn = sc.next();
                 System.out.println("Hvad er dens superheltenavn?");
                 String superheroname = sc.next();
 
-                //
+
                 System.out.println("Er superhelten et menneske? yes/no");
-                String svar=sc.next();
-                switch(svar){
+                String svar = sc.next();
+                switch (svar) {
                     case "yes":
                         isHuman = true;
                         break;
                     case "no":
-                        isHuman=false;
+                        isHuman = false;
                         break;
                     default:
                         System.out.println("Switch-case passed to default state.");
@@ -49,26 +51,36 @@ public class Main {
                 double strength = sc.nextDouble();
                 System.out.println("Okay! Din superhelt er kreeret");
                 database.createSuperhero(navn, superheroname, isHuman, creationyear, strength);
-            } else if (valg==5) {
+
+
+                // SØGE FUNKTION
+            } else if (valg == 2) {
+                // Skal finde ud af hvordan jeg tager det første søgeresultat (første index) og får denne skrevet ud.
+                System.out.println("Indtast navn/superheltenavn på den superhelt du ønsker at få vist");
+                String search = sc.next();
+               database.searchSuperhero(search);
+
+            }
+            // LISTE UDSKRIVES
+            else if (valg == 5) {
                 System.out.println("Din liste over superhelte ser således ud:");
-                for(Superhero superheroes : database.superheroes){
-                    System.out.println("Rigtige navn: "+superheroes.getName());
-                    System.out.println("Superheltenavn: "+superheroes.getSuperheroName());
+                for (Superhero superheroes : database.superheroes) {
+                    System.out.println("Rigtige navn: " + superheroes.getName());
+                    System.out.println("Superheltenavn: " + superheroes.getSuperheroName());
                     // LØST MED EN IF ELSE, ER DER MON EN ANDEN GOD MÅDE AT GØRE DET PÅ?
-                        if(isHuman!=true){
-                            System.out.println("Er menneske: Nej");
-                        } else{
-                            System.out.println("Er menneske: Ja");
-                        }
-                    System.out.println("Superhelten er lavet i år: "+superheroes.getCreationYear());
-                    System.out.println("Superheltens styrke er: "+superheroes.getStrength());
+                    if (isHuman != true) {
+                        System.out.println("Er menneske: Nej");
+                    } else {
+                        System.out.println("Er menneske: Ja");
+                    }
+                    System.out.println("Superhelten er lavet i år: " + superheroes.getCreationYear());
+                    System.out.println("Superheltens styrke er: " + superheroes.getStrength());
                     System.out.println(" ");
                 }
-            }else if(valg==9){
+                // PROGRAMMET AFSLUTTES
+            } else if (valg == 9) {
                 System.out.println("Programmet afsluttes.");
             }
-        } while (valg == 1 || valg == 5);
-
-
+        } while (valg == 1 || valg == 5 || valg == 2);
     }
 }
